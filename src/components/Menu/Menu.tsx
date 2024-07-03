414814303f0829c3ae3964a3882c3f7c79b02dba
riyaz <vriyazzz1001@gmail.com>
2024-07-04 02:52:31 +0300

    add menu

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Menu = () => {
+  return <div className="menu">Menu</div>;
+};
--- src/components/Streamers/Streamers.tsx
+++ src/components/Streamers/Streamers.tsx
@@ -1,9 +1,15 @@
 import { StreamerView } from "../StreamerView/StreamerView";
-
+import ".//Streamers.scss";
 export const Streamers = () => {
   const streamers = [1, 2, 3, 4, 5, 6];
   return (
-    <div className="section">
+    <div className="section streamers">
+      <p className="header-text">Стримеры</p>
+      <p className="details-text">
+        Подпишитесь на стримера, чтобы участвовать в его розыгрышах и получать
+        уведомления от него
+      </p>
+
       {streamers.map((s) => (
         <StreamerView></StreamerView>
       ))}

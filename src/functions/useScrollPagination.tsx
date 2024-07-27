91cd10efacb59249608db8b935fa75a5612f46a7
riyaz <vriyazzz1001@gmail.com>
2024-07-28 00:37:40 +0300

    --all

--- src/functions/useScrollPagination.tsx
+++ src/functions/useScrollPagination.tsx
@@ -1,14 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const useScrollPagination = () => {
   const [page, setPage] = useState(1);
   const [pageSize, setPageSize] = useState(20);
+  const lastScrollTimeRef = useRef<number>(0);
   const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
     const target = e.currentTarget;
     const { scrollTop, scrollHeight, clientHeight } = target;
 
     if (scrollHeight - scrollTop <= clientHeight + 100) {
-      setPage((prevPage) => prevPage + 1);
+      const now = Date.now();
+      if (now - lastScrollTimeRef.current > 2000) {
+        setPage((prevPage) => prevPage + 1);
+        lastScrollTimeRef.current = now;
+      }
     }
   };
   useEffect(() => {

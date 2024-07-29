7d1d7f58882d884a73b74ab33a1cf31fc2e982a9
riyaz <vriyazzz1001@gmail.com>
2024-07-29 12:04:04 +0300

    -A

--- src/components/withMenu/withMenu.tsx
+++ src/components/withMenu/withMenu.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Menu } from "../Menu/Menu";
 import "./withMenu.scss";
-
+import { useLocation } from "react-router-dom";
 import useApplyTelegramTheme from "../../functions/useApplyTelegramTheme";
 export const WithMenu = ({ children }: { children: React.ReactNode }) => {
+  const location = useLocation();
+  useEffect(() => {
+    const elem = document.querySelector(".footer") as HTMLDivElement;
+    if (elem != null) {
+      const section = document.querySelector(".section") as HTMLDivElement;
+      section.style.minHeight = `calc(100vh - ${elem.offsetHeight}px)`;
+    }
+  }, [location]);
   useApplyTelegramTheme();
   return (
     <>

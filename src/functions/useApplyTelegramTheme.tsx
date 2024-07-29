75cd667bc50eddfcc590bff756010920be93cff1
riyaz <vriyazzz1001@gmail.com>
2024-07-29 11:22:31 +0300

    -A

--- src/functions/useApplyTelegramTheme.tsx
+++ src/functions/useApplyTelegramTheme.tsx
@@ -5,14 +5,18 @@ import { useNavigate } from "react-router-dom";
 const useApplyTelegramTheme = () => {
   const navigate = useNavigate();
   useEffect(() => {
-    const onClick = () => navigate(-1);
-    tg.BackButton.onClick(onClick).show();
-    tg.expand();
-    tg.disableVerticalSwipes();
-    tg.enableClosingConfirmation();
-    tg.setHeaderColor("#fff");
-    tg.setBackgroundColor("#fff");
-    console.log(tg.isVerticalSwipesEnabled, "f");
+    try {
+      const onClick = () => navigate(-1);
+      tg.BackButton.onClick(onClick).show();
+      tg.expand();
+      tg.disableVerticalSwipes();
+      tg.enableClosingConfirmation();
+      tg.setHeaderColor("#fff");
+      tg.setBackgroundColor("#fff");
+      console.log(tg.isVerticalSwipesEnabled, "f");
+    } catch (error) {
+      console.log(error);
+    }
   }, []);
 };
 

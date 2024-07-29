db54d5d1ead01b54576839560950e7a82cd3dd8b
riyaz <vriyazzz1001@gmail.com>
2024-07-29 15:15:31 +0300

    -A

---
+++ src/functions/useDisableScroll.tsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+
+export const useDisableBounces = (classname: string) => {
+  useEffect(() => {
+    const streamersEl = document.querySelector(
+      `.${classname}`
+    ) as HTMLDivElement;
+    const canScroll = (el: HTMLElement): boolean => {
+      return el.scrollHeight > el.clientHeight;
+    };
+    const onScroll = (e: TouchEvent) => {
+      if (e.target === streamersEl) {
+        e.preventDefault();
+      } else {
+        let target = e.target as HTMLElement;
+        while (target && target !== streamersEl) {
+          if (canScroll(target)) {
+            return;
+          }
+          target = target.parentNode as HTMLElement;
+        }
+        e.preventDefault();
+      }
+    };
+    streamersEl.addEventListener("touchmove", onScroll, { passive: false });
+    return () => {
+      streamersEl.removeEventListener("touchmove", onScroll);
+    };
+  }, []);
+};

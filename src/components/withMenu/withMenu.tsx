d135083133e7f5da849b5067f79caa7836a2943d
riyaz <vriyazzz1001@gmail.com>
2024-07-29 11:38:22 +0300

    -A

--- src/components/withMenu/withMenu.tsx
+++ src/components/withMenu/withMenu.tsx
@@ -1,17 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Menu } from "../Menu/Menu";
 import "./withMenu.scss";
-import { useLocation } from "react-router-dom";
+
 import useApplyTelegramTheme from "../../functions/useApplyTelegramTheme";
 export const WithMenu = ({ children }: { children: React.ReactNode }) => {
-  const location = useLocation();
-  const [height, setHeight] = useState(0);
-  useEffect(() => {
-    const elem = document.querySelector(".footer") as HTMLDivElement;
-    if (elem != null) setHeight(elem.offsetHeight);
-    const root = document.querySelector("#root") as HTMLDivElement;
-    root.style.paddingBottom = `${elem.offsetHeight}px`;
-  }, [location]);
   useApplyTelegramTheme();
   return (
     <>
